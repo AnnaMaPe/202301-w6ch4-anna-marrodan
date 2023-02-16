6cb918dcb44ae0776b63d14e4a4bfc00cb1c5d8d
Anna Marrodán <anna.marrodan@gmail.com>
2023-02-16 20:51:36 +0100

    Create function to obtain a thing I know by ID

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import "./loadEnvironment.js";
 import express from "express";
-import getThingsIKnow from "./controllers/thingsIKnowControllers.js";
+import { getThingsIKnow } from "./controllers/thingsIKnowControllers.js";
+import { getThingIKnowById } from "./controllers/thingsIKnowControllers.js";
 
 const app = express();
 const port = process.env.PORT ?? 4000;
 
 app.get("/things", getThingsIKnow);
+app.get("/things/:id", getThingIKnowById);
 
 app.listen(port);
